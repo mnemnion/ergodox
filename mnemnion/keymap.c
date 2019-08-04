aaa9f02b88647cfaaa61d500c64a3126d52e3e83
Sam Atman <atmanistan@gmail.com>
2019-08-04 03:47:20 -0400

    de-nerf left layer key + remove some combos

--- mnemnion/keymap.c
+++ mnemnion/keymap.c
@@ -39,10 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_ergodox(
     // left hand
     KC_EQUAL, KC_1, KC_2, KC_3, KC_4, KC_5, SWITCH_WIN,
-    KC_MINUS, KC_Q, KC_W, KC_E, KC_R, KC_T, _______,
+    KC_MINUS, KC_Q, KC_W, KC_E, KC_R, KC_T, TG(2),
     KC_BSPACE, KC_A, KC_S, KC_D, KC_F, KC_G,
     OSM(MOD_LSFT), CTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_LGUI,
-     LT(1,KC_GRAVE), KC_BSLASH, KC_LGUI, KC_LEFT, KC_RIGHT,
+     KC_GRAVE, KC_BSLASH, KC_LGUI, KC_LEFT, KC_RIGHT,
 
     // left thumb
     	KC_LCTRL, LALT_T(KC_ESCAPE),
@@ -52,12 +52,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
     BSWITCH_WIN, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DELETE,
     TG(1), KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLASH,
-    KC_H, KC_J, KC_K, KC_L, LT(2,KC_SCOLON),KC_QUOTE,
+    KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
     KC_RGUI, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT,
      KC_UP, KC_DOWN, KC_LBRACKET, KC_RBRACKET, MO(1),
 
     // right thumb
-    KC_RALT, KC_RCTRL,
+    RALT_T(KC_ESCAPE), KC_RCTRL,
     KC_PGUP,
     KC_PGDOWN, KC_TAB, KC_ENTER
 ),
